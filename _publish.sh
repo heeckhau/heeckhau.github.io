e5bfc992383978d967eb69f2d96b9cee1d3314d7
Hendrik Eeckhaut <hendrik.eeckhaut@sigasi.com>
2017-01-02 10:49:30 +0100

    publish script

--- _publish.sh
+++ _publish.sh
@@ -9,6 +9,14 @@
 rm -rf _build || exit -1
 git worktree prune || exit -1
 
+# check that there are no uncommited changes
+if ! git diff-index --quiet HEAD --
+    then
+        echo >&2 "You have uncommited changes."
+        git diff-files --name-status -r --ignore-submodules -- >&2
+        exit -1
+fi
+
 VERSION_HASH=$(git rev-parse --short HEAD)
 
 echo "Fetch and checkout latest version from Assembla"
